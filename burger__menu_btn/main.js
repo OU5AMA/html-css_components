88e52496ddc8aeabf882f0d1af8c8dc17307951f
OU5AMA <lasri.oussama@gmail.com>
2022-02-21 23:54:29 +0100

    adding a burger menu

---
+++ burger__menu_btn/main.js
@@ -0,0 +1,15 @@
+// const burgerMenu = document.querySelector('.burgerMenu__bar');
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen){
+        menuBtn.classList.add('open');
+        menuOpen = true;
+        console.log(menuOpen)
+    }else{
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+        console.log(menuOpen)
+
+    }
+});
